a7ab0dfd124f1d0af6bcca232871d983f3edff6a
devjunayed <90998396+devjunayed@users.noreply.github.com>
2022-02-20 22:58:53 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var qrcode = document.getElementById('qrcode');
 var input = document.getElementById('input');
 var generateBtn = document.getElementById('generate');
 var download = document.getElementById('download');
-
+var excl = document.getElementById('exclamation');
 
 generateBtn.addEventListener("click", () => {
     var url = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${input.value}`;
@@ -23,3 +23,27 @@ function getFileName(str){
     return str.substring(str.lastIndexOf('/') + 1);
 }
 
+// excl.addEventListener("click", ()=>{
+//     alert("you fool this is a just joke button")
+// })
+
+
+
+
+// modal
+
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
+
+excl.onclick = function(){
+    modal.style.display = "block";
+}
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
